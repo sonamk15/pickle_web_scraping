45c2593bf3a86c7b4e3209aa42a2287fb14f9a7a
Komal Bhatt <komal18@navgurukul.org>
2019-06-02 10:26:00 +0530

    update

--- pickle_scraping.py
+++ pickle_scraping.py
@@ -6,12 +6,13 @@ soup = BeautifulSoup(page.text, "html.parser")
 # div = soup.findAll('div',class_="_3RA-")
 div = soup.findAll("div",class_="_1fje")
 for i in div:
+    poster = i.findAll('div' ,class_="_3nWP" )
+    for url in poster:
+        img_url =  url.find('img',src=True)['src']
+    # print (i.prettify()
     name=i.findAll('div',class_="_2apC")
-    # name=i.findAll('div',class_="_1fje")
     for j in name:
         print (j.text)
-        # k = j.findAll('div',class_="_2apC")
-        # for x in k:
-        #     print (x.text)
-    # print (i.prettify())
-# for i in div:
+        
+    price = i.find('div',class_="_1kMS").text
+    # print (price) 
